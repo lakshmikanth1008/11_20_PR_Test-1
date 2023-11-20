c900380c8588edf2b7561eab296ef051510b55ee
lakshmikanth1008 <58290768+lakshmikanth1008@users.noreply.github.com>
2023-11-20 18:37:28 -0500

    commit

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import pandas as pd
+import altair as alt
+
+
 
 trafficking_df = pd.read_csv('Trafficking_Data.csv')
 
 st.dataframe(trafficking_df)
+
+chart1=alt.Chart(trafficking_df).mark_bar().encode(
+    x='gender',
+    y='count()'
+)
+
+st.altair_chart(chart1, use_container_width=True)
